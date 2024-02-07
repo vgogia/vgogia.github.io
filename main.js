3cc3dc496ed50ac0fb44468b31848fe10d4bd80e
vgogia <vgogia@torontomu.ca>
2024-02-07 00:01:25 -0500

    commit

---
+++ main.js
@@ -0,0 +1,9 @@
+var username = prompt("What is your name?")
+alert("Welcome" + username + "to my website!")
+
+/*1. Ask your users to enter their names when your website is loaded.
+2. Store that name in a variable, which can be displayed on your website.
+3. Display a welcome message on your website with the name included.
+4. Change the style of at least two types of DOM elements (i.e. fonts, colour, etc).
+5. Use the innerHTML property to change the text of one of your headings.
+6. Include comments for all the different functionality in your file /*
