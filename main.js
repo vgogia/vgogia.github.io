1429feb6bf03ff4bf831837133cc59d08ebac622
vgogia <vgogia@torontomu.ca>
2024-02-24 01:43:48 -0500

    commit L5

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
-var username = prompt("What is your name?"); // Ask user to enter name and store in variable 'username'
-alert("Welcome " + username + " to my website!"); // Display welcome message with user's name
+function enterName(){
+    var username = prompt("What is your name?");
+    var h1Element = document.querySelector("h1");
+    h1Element.innerHTML = "Hello, " + username + "! I am Varun Gogia.";
+}
 
-window.onload = function() {
-    var h1Element = document.querySelector("h1"); // Select the H1 element and store in variable
-    h1Element.style.fontFamily = "Times New Roman"; // Change DOM element font
-    h1Element.style.backgroundColor = "cyan"; // Change DOM element H1 backgound colour
-    h1Element.innerHTML = "I am Varun Gogia"; // Use innerHTML to change text of main heading H1
-};
+function imageClicked(){
+    var imgElement = document.getElementById("mepic");
+    imgElement.classList.toggle("enlargePic");
+}
+
+function navHover(element) {
+    element.style.color = "#42f5b6";
+}
+
+function navNoHover(element) {
+    element.style.color = "";
+}
