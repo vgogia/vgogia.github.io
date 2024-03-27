a2b273b309f2c492ba6525bd6f7095240e7c8a8e
vgogia <vgogia@torontomu.ca>
2024-03-26 22:47:25 -0400

    L8 Commit

--- main.js
+++ main.js
@@ -32,3 +32,40 @@ window.onclick = function(event) {
     }
     }
     }
+
+// Accessibility
+function fontSize(size) {
+    var body = document.body;
+    if(size === 'small') {
+        body.style.fontSize = '10px'; // smaller size
+    } else if(size === 'normal') {
+        body.style.fontSize = '16px'; // regular size
+    } else if(size === 'large') {
+        body.style.fontSize = '22px'; // large size
+    }
+    }
+
+var defaultBG = false; // current color
+function colourToggle() {
+    var body = document.body;
+    var main = document.querySelector('.main');
+    var social = document.querySelector('.socials');
+    var main2 = document.querySelector('.main2');
+    defaultBG = !defaultBG; // Toggle the flag
+    
+    if (defaultBG) {
+        // colours when toggled
+        body.style.backgroundColor = "#93B7DC";
+        body.style.color = "#000000";
+        main.style.backgroundColor = "#CECECE";
+        social.style.backgroundColor = "#ffffff";
+        main2.style.backgroundColor = "ffffff";
+    } else {
+        // revert
+        body.style.backgroundColor = "#053c5e";
+        body.style.color = "#000000";
+        main.style.backgroundColor = "#bfdbf7";
+        social.style.backgroundColor = "#dce0d9";
+        main2.style.backgroundColor = "#bfdbf7";
+    }
+}
